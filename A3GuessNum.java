3d0fd7febe05053c62c451ad899c8a915d9cb942
Rachael McIlwrath <92391423+RachaelMcI@users.noreply.github.com>
2021-11-29 04:49:00 +0000

    cleaned up the base: "error free", just for handing it in for our first due date on november 28. Removed { on 93 & "again"

--- A3GuessNum.java
+++ A3GuessNum.java
@@ -14,23 +14,22 @@ public class A3GuessNum{
     // Make a scanner and Random Object.
     Scanner input = new Scanner(System.in);
     Random numGenerator = new Random();
-    boolean inputValid = true;
-    System.out.println("Would you like to play? Yes/No");
-    String playAgain = input.next();
-    while(playAgain.equals("yes")||playAgain.equals("Yes")||playAgain.equals("y") && inputValid == true){
+    
     // Generate a random number from 0-5.
     int answer = numGenerator.nextInt(200) + 1;
     int guess = 0; // default guess num.
-    int counter = 0;
+    int counter = 0; //keeps track of guesses
     // main code
-     // could be in the wrong area, or changed into a while    loop
-      System.out.println("Guess a number between 1 and 200.");
+    System.out.println("Would you like to play? Yes/No");
+    String playAgain = input.next();
+    if (playAgain.equals("yes")||playAgain.equals("Yes")||playAgain.equals("y")){ // could in a loop, see "working towards challenge 3 " commit.
+      System.out.println("Guess a number.");
      while(guess != answer && counter <= 4 && input.hasNextInt()){ //while as a specific condition ends it
        guess = input.nextInt();  
        counter++;
        
           //hints that can be moved into a method
-         if(guess >= answer+55 || guess<= answer-55){ // is there a more effective way of doing this
+         if(guess >= answer+55 || guess<= answer-55){ 
            System.out.println("Hint: You're freezing!");
            System.out.println("Attempts:" + counter);
              }
@@ -38,110 +37,36 @@ public class A3GuessNum{
               System.out.println("Hint: You're cold.");
               System.out.println("Attempts:" + counter);
                }
-           else if(guess>=answer+10 || guess <= answer-10){ // should be 10 from the answer --> answer-10
+           else if(guess>=answer+10 || guess <= answer-10){ 
                System.out.println("Hint: you're warm.");
                System.out.println("Attempts:" + counter);
                }
-            else if(guess>=answer+5 || guess <= answer-5){ // should be 10 from the answer        --> answer+5
+            else if(guess>=answer+5 || guess <= answer-5){ // a range from the answer
                 System.out.println("Hint: Hot, hot, hot!");
                 System.out.println("Attempts:" + counter);
              }
       }
       //ENDWHILE
-         if(guess == answer){ //maybe change to attemptCount(er)
+           if(guess == answer){ 
             System.out.println("Congratulations!");
-            System.out.println("Would you like to play again? Yes/No");
-            input.next();
-            //you guessed in ___ tries
-          }
+            //can add confettii in the future
+           }
           else if(counter > 5){
             System.out.println("No Tries left. You lose, sorry!");
-            System.out.println("Would you like to play again? Yes/No");
-            playAgain.next();
-            if(playAgain.equals("no")||playAgain.equals("No")||playAgain.equals("n")||input.hasNextInt()){
-             inputValid = false;
-             System.out.println("Bye");
           }
-           else{ //could make a seperate string method for confetti
+           else{ 
              System.out.println("Input Error!");
-             System.out.println("Would you like to play again? Yes/No");
-             playAgain.next();
-             if(playAgain.equals("no")||playAgain.equals("No")||playAgain.equals("n")||input.hasNextInt()){
-               inputValid = false;
-               System.out.println("Bye");
-             }
-             else{
-               inputValid = true;
-             }
             } 
-  
       }
-       if(playAgain.equals("no")||playAgain.equals("No")||playAgain.equals("n")){
+      else if(playAgain.equals("no")||playAgain.equals("No")||playAgain.equals("n")){
           System.out.println("bye.");
-           inputValid = false;
          }
       else{
         System.out.println("Invalid Input");
-           inputValid = false;
       }
   }// close main
 }
-/*public class Main {
 
-    public static void main(String[] args) {
-        // how to create a scanner
-     Scanner input = new Scanner(System.in);   
-     //random object creator 
-     Random randomNumGenerator = new Random();
-     //set between 1 & 20
-    int randomNum = randomNumGenerator.nextInt(20) + 1;
-    int guessNum = 0;
-    
-    while(guessNum != randomNum){
-        System.out.println(guessNum);
-        System.out.println("Guess a number");
-        guessNum = input.nextInt();
 
-        }
-    }
-}
-*/
-
-// remember to include to display counter of how many tries are left
-// remember to include a check for any incorrect inputs, should have something on chromebook.
-//could add a keep playing? / forefiet? 
-//arra
-
-//}
-/*public class Main {
-
-    public static void main(String[] args) {
-        // how to create a scanner
-     Scanner input = new Scanner(System.in);   
-     //random object creator 
-     Random randomNumGenerator = new Random();
-     //set between 1 & 20
-    int randomNum = randomNumGenerator.nextInt(20) + 1;
-    int guessNum = 0;
-    
-    while(guessNum != randomNum){
-        System.out.println(guessNum);
-        System.out.println("Guess a number");
-        guessNum = input.nextInt();
-
-        }
-    }
-}
-*/
-
-// remember to include to display counter of how many tries are left
-// remember to include a check for any incorrect inputs, should have something on chromebook.
-//could add a keep playing? / forefiet? 
-//arra
-
-/* can restore to yesterday, would need to change while statement & comment out array  else if(playAgain.equals("no")||playAgain.equals("No")||playAgain.equals("n")){
-          System.out.println("bye.");
-         }
-       else{
-        System.out.println("Invalid Input");
-        } */
+// optional:expand on the strings & use playAgain.next(); more 
+//could add a keep playing / forefiet 
